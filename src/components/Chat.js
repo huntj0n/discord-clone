08faa74c93aaf52d3cde9769e10bd47319af77db
huntj0n <hunt.jon19@gmail.com>
2020-10-11 15:50:12 -0600

    read.me update

---
+++ src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chat.css';
+
+function Chat() {
+    return (
+        <div>
+            this is the chat component
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            this is the sidebar
+
+            <div className="sidebar__top">
+                <h3>Discord-Clone</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
