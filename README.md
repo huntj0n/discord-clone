0981778e73551738ced5dfa170f9cd054450d83e
huntj0n <hunt.jon19@gmail.com>
2020-10-11 17:27:11 -0600

    sidebar

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This project was bootstrapped with Create-React-App using the Redux template
+
+# Dependencies Used
+    -   Redux
+    -   React Redux
+    -   Material-UI Icons
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import './Sidebar.css'
+import './Sidebar.css';
+
+import SidebarChannel from './SidebarChannel';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            this is the sidebar
-
             <div className="sidebar__top">
                 <h3>Discord-Clone</h3>
+                <ExpandMoreIcon />
+            </div>
+
+            <div className="sidebar__channels">
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon />
+                        <h4>Text Channels</h4>
+                    </div>
+
+                    <AddIcon 
+                        className="sidebar__addChannel"
+                    />
+                </div>
+                <div className="sidebar__channelsList">
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                </div>
+            </div>
+            <div className="sidebar__voice">
+                <SignalCellularAltIcon 
+                    className="sidebar__voiceIcon"
+                    fontSize="large"
+                />
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlinedIcon /> 
+                    <CallIcon/>
+                </div>
             </div>
         </div>
     )
